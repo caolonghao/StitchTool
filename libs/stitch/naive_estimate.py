e528a4ee8912db0d297cbbae70b500e4621b7001
TonyCao <tonycao0918@qq.com>
2023-05-06 14:57:52 +0800

    update naive estimate

--- libs/stitch/naive_estimate.py
+++ libs/stitch/naive_estimate.py
@@ -3,17 +3,47 @@ from skimage import io
 import numpy as np
 
 
+def im2col(img, kernel_size, stride=1):
+    pad = (kernel_size - 1) // 2  # 默认stride为1
+
+    H, W = img.shape
+    out_h = (H + 2 * pad - kernel_size) // stride + 1
+    out_w = (W + 2 * pad - kernel_size) // stride + 1
+
+    img = np.pad(
+        img, [(pad, pad), (pad, pad)], "edge"
+    )  # padding部分的mean、std是有影响的，是否需要单独拉出来算？
+    col = np.zeros((kernel_size, kernel_size, out_h, out_w))
+
+    for y in range(kernel_size):
+        y_max = y + stride * out_h
+        for x in range(kernel_size):
+            x_max = x + stride * out_w
+            col[y, x, :, :] = img[y:y_max:stride, x:x_max:stride]
+    col = col.transpose(2, 3, 0, 1).reshape(out_h * out_w, -1)
+    return col
+
+
 class NaiveEstimate:
     # windows size is recommended to be an odd number
-    def __init__(self, debug=False, window_size=5) -> None:
+    def __init__(self, debug=False, window_size=5, select_scale_factor=0.5) -> None:
         self.debug = debug
         self.window_size = window_size
+        self.select_scale_factor = select_scale_factor
 
-    def __call__(self, img_stack, pre_gaussian_filter_sigma=0.6):
+    def image_deviation_select(self, images, scale_factor=0.5):
+        reshaped_images = images.reshape(images.shape[0], -1)
+        std_deviation = np.std(reshaped_images, axis=1)
+        threshold = std_deviation.min() * (1.0 + scale_factor)
+        valid_pos = std_deviation <= threshold
+
+        return images[valid_pos]
+
+    def __call__(self, img_stack, gaussian_sigma=0.6):
         # gaussian filter
-        if pre_gaussian_filter_sigma > 0:
-            img_stack = gaussian_filter(img_stack, sigma=pre_gaussian_filter_sigma)
-            print("gaussian filter sigma={:.1f} done".format(pre_gaussian_filter_sigma))
+        if gaussian_sigma > 0:
+            img_stack = gaussian_filter(img_stack, sigma=gaussian_sigma)
+            print("gaussian filter sigma={:.1f} done".format(gaussian_sigma))
 
         # remove added all-zero image
         new_img_stack = []
@@ -35,7 +65,10 @@ class NaiveEstimate:
 
         # TODO:后续可以考虑在平滑度和亮度间做一个更好的决策,比如加权得分排序之类的；或者在最平滑的中选最暗的？
         dark_img_nums = int(reconstructed_imgs.shape[0] * 0.1)
-        for i, img in enumerate(reconstructed_imgs[-dark_img_nums:][::-1]):
+        valid_dark_imgs = self.image_deviation_select(
+            reconstructed_imgs[-dark_img_nums:], self.select_scale_factor
+        )
+        for i, img in enumerate(valid_dark_imgs):
             bgs.append([img, self.LCoV_evaluate(img), i])
 
         bgs.sort(key=lambda x: x[1])
@@ -81,9 +114,11 @@ class NaiveEstimate:
 
         # TODO: evaluate 相对较慢，可以考虑减少选取的图片量
         # 现在通过实验发现大部分最优点都在30%之前，只有极少数的会出现在前50%
-        bright_img_nums = int(reconstructed_imgs.shape[0] * 0.50)
-        for i, img in enumerate(reconstructed_imgs[:bright_img_nums]):  # 取前85张图，找其中最平滑的
-            img -= bg
+        bright_img_nums = int(reconstructed_imgs.shape[0] * 0.6)
+        valid_bright_imgs = self.image_deviation_select(
+            reconstructed_imgs[:bright_img_nums] - bg, self.select_scale_factor
+        )
+        for i, img in enumerate(valid_bright_imgs):
             distortions.append([img, self.LCoV_evaluate(img), i])
 
         # 平滑度阈值不超过最低的 0.5%
@@ -125,24 +160,3 @@ class NaiveEstimate:
         LCoV_sum = np.sum(np.std(cols, axis=1) / np.mean(cols, axis=1))
 
         return LCoV_sum
-
-
-def im2col(img, kernel_size, stride=1):
-    pad = (kernel_size - 1) // 2  # 默认stride为1
-
-    H, W = img.shape
-    out_h = (H + 2 * pad - kernel_size) // stride + 1
-    out_w = (W + 2 * pad - kernel_size) // stride + 1
-
-    img = np.pad(
-        img, [(pad, pad), (pad, pad)], "edge"
-    )  # padding部分的mean、std是有影响的，是否需要单独拉出来算？
-    col = np.zeros((kernel_size, kernel_size, out_h, out_w))
-
-    for y in range(kernel_size):
-        y_max = y + stride * out_h
-        for x in range(kernel_size):
-            x_max = x + stride * out_w
-            col[y, x, :, :] = img[y:y_max:stride, x:x_max:stride]
-    col = col.transpose(2, 3, 0, 1).reshape(out_h * out_w, -1)
-    return col
